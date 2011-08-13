bf671b4f897f7690cf6972636dce7cdb69bd6865
Edwin Flores <edwin@flores.geek.nz>
2011-08-13 04:08:36 +0000

    Test?

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,14 @@
+// OpenCV stuff
+#include <cv.h>
+#include <highgui.h>
+// OpenCV/Kinect compat
 #include "libfreenect_cv.h"
 
 int main() {
+	cvNamedWindow("depth");
+
+	while (cvWaitKey(10) != 27) {
+		IplImage *img = freenect_sync_get_depth_cv(0);
+		cvShowImage("depth", img);
+	}
 }
