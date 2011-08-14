e96291b9609b4a59a9ec0f4133b77c65035b0fe2
Edwin Flores <edwin@flores.geek.nz>
2011-08-14 05:17:52 +0200

    Added ColorPoint::ColorPoint definition and tidied up

--- src/geometry.cpp
+++ src/geometry.cpp
@@ -5,31 +5,38 @@
 #define XWIDTH 5.4
 
 
-ColorPoint::ColorPoint(float newX,float newY,float newZ, float newR, float newG, float newB):Point(newX, newY,newZ),r(newR), g(newG),b(newB) {
+ColorPoint::ColorPoint(float newX,float newY,float newZ, float newR, float newG, float newB)
+	: Point(newX, newY,newZ),r(newR), g(newG),b(newB) {
 
 }
 
-ColorPoint::ColorPoint(int x, int y, int d, float r, float g, float b): Point(x, y, d), r(r), g(g), b(b) {
+ColorPoint::ColorPoint(int x, int y, int d, float r, float g, float b)
+   	: Point(x, y, d), r(r), g(g), b(b) {
 }
 
-Vector::Vector(float newDistanceX,float newDistanceY,float newDistanceZ) : distanceX(newDistanceX),distanceY(newDistanceY),distanceZ(newDistanceZ) {
-	
+ColorPoint::ColorPoint() : Point(), r(0), g(0), b(0) {
 }
 
 Point::Point(float newX,float newY,float newZ) : x(newX), y(newY),z(newZ) {
 	
 	
 }
-Point::Point() {
-	
-	
-}
+
 Point::Point(int x, int y, int d) {
 	double ywidth = XWIDTH * YRES / XRES;
 
 	this->x = ((XWIDTH * x) / (XRES - 1)) - (XWIDTH / 2);
 	this->y = ((ywidth * y) / (YRES - 1)) - (ywidth / 2);
 	this->z = DIST;
+}
+
+Point::Point() : x(0), y(0), z(0) {
+	
+}
+
+Vector::Vector(float newDistanceX,float newDistanceY,float newDistanceZ)
+	: distanceX(newDistanceX),distanceY(newDistanceY),distanceZ(newDistanceZ) {
+	
 }
 
 
